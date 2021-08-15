03d9cc9464ec2322762e7e47dad14c367ca2ee98
Chandler Liang <chandler.liang123@gmail.com>
2021-08-15 14:53:21 +1000

    updated query.sql

--- db/query.sql
+++ db/query.sql
@@ -5,7 +5,13 @@ SELECT role.id, role.title, role.salary, department.name
     JOIN department ON role.department_id = department.id
     ORDER BY role.id ASC;
 
-SELECT *
+SELECT  employee.id AS ID,
+        employee.first_name AS `FIRST NAME`,
+        employee.last_name AS `LAST NAME`,
+        employee.manager_id,
+        role.title AS `TITLE`,
+        role.salary AS `SALARY`,
+        department.name AS `DEPARTMENT`
     FROM employee
     INNER JOIN role ON employee.role_id = role.id
     INNER JOIN department ON role.department_id = department.id;
