8ed485588b576c4a27f07a7051b0e92646f3369e
Chandler Liang <chandler.liang123@gmail.com>
2021-08-18 17:27:12 +1000

    added all view options

--- scripts/EmployeeData.js
+++ scripts/EmployeeData.js
@@ -41,10 +41,54 @@ class EmployeeData {
         });
     }
     
+    viewThroughManager(managerID) {
+        query(`SELECT  employee.id AS ID,
+        employee.first_name AS "FIRST NAME",
+        employee.last_name AS "LAST NAME",
+        role.title AS "TITLE",
+        role.salary AS "SALARY",
+        department.name AS "DEPARTMENT",
+        CONCAT(manager.first_name, " ", manager.last_name) AS "MANAGER NAME"
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    INNER JOIN role ON employee.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id
+    WHERE employee.manager_id = ?`, managerID, function (err, results) {
+        console.table('\n', results);
+        });
     
+    }
+
+    viewThroughDepartment(departmentID) {
+        query(`SELECT  employee.id AS ID,
+        employee.first_name AS "FIRST NAME",
+        employee.last_name AS "LAST NAME",
+        role.title AS "TITLE",
+        role.salary AS "SALARY",
+        department.name AS "DEPARTMENT",
+        CONCAT(manager.first_name, " ", manager.last_name) AS "MANAGER NAME"
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    INNER JOIN role ON employee.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id
+    WHERE department.id = ?`, departmentID, function (err, results) {
+        console.table('\n', results);
+        });
+    }
+
+    viewDepartmentBudget(departmentID) {
+        query(`SELECT SUM(salary) AS "TOTAL DEPARTMENT BUDGET"
+        FROM employee
+        LEFT JOIN employee manager ON manager.id = employee.manager_id
+        INNER JOIN role ON employee.role_id = role.id
+        INNER JOIN department ON role.department_id = department.id
+        WHERE department.id = ?`, departmentID, function (err, results) {
+            console.table('\n', results);
+        });
+    }
 
 }
 
 const test = new EmployeeData();
 
-test.viewAllEmployees();
+test.viewDepartmentBudget(1);
