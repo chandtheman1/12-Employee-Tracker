0b130d0ddf487395ffd6e8f6d85f99a06e2a8207
Chandler Liang <chandler.liang123@gmail.com>
2021-08-19 15:50:22 +1000

    inquirer issues

--- index.js
+++ index.js
@@ -15,13 +15,6 @@ async function actionPrompt() {
             type: 'list',
             choices: [
                 "View All Employees",
-                "Add Employee",
-                "Update Employee Role",
-                "View All Roles",
-                "Add Role",
-                "View All Departments",
-                "Add Department",
-                "Quit"
             ] 
         }
     ])
